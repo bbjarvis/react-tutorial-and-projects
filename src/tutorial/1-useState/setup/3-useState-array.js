032f20ea95334356f3bf58f3f3a36ccddb69726c
Brett Jarvis <brettbjarvis@gmail.com>
2023-01-27 14:02:33 -0800

    refactor for inline fxn to remove people

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import { data } from '../../../data';
+import React from 'react'
+import { data } from '../../../data'
 
 const UseStateArray = () => {
   const [people, setPeople] = React.useState(data)
 
-  const removeItem = (id) => {
-    let newPeople = people.filter((person) => person.id !== id)
-    setPeople(newPeople)
-  }
-
-  return <>
-      {
-      people.map(person => {
-        const {id, name} = person;
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name } = person
         return (
-        <div key={id} className='item'>
-          <h4>{name}</h4>
-          <button onClick={() => removeItem(id)}>remove</button>
-
-        </div>)
-      }
-      )}
+          <div key={id} className='item'>
+            <h4>{name}</h4>
+            <button
+              onClick={() =>
+                setPeople(people.filter((person) => person.id !== id))
+              }
+            >
+              remove
+            </button>
+          </div>
+        )
+      })}
       <button className='btn' onClick={() => setPeople([])}>
         clear items
       </button>
-        
     </>
-};
+  )
+}
 
-export default UseStateArray;
+export default UseStateArray
