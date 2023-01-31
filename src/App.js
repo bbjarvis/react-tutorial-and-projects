f1e3674113c4d65fd9bf4c35ae7a4afae423b9ff
Brett Jarvis <brettbjarvis@gmail.com>
2023-01-30 17:06:48 -0800

    add fxnality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react'
-import Setup from './tutorial/1-useState/setup/3-useState-array'
-import Final from './tutorial/1-useState/final/3-useState-array'
+import Setup from './tutorial/1-useState/setup/4-useState-object'
+import Final from './tutorial/1-useState/final/4-useState-object'
 function App() {
   return (
     <div className='container'>
       <Setup />
-      {/* <Final /> */}
+      <Final />
     </div>
   )
 }
 
 export default App
-
--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
-};
+  const [person, setPerson] = useState({
+    name: 'peter',
+    age: 24,
+    message: 'random message',
+  })
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h4>{person.message}</h4>
+      <button
+        className='btn'
+        onClick={() => setPerson({ ...person, message: 'hello worlk' })}
+      >
+        Change Message
+      </button>
+    </>
+  )
+}
 
-export default UseStateObject;
+export default UseStateObject
