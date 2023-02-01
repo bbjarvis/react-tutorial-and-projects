90a304ec02f600dae6ba9ce18eedd7c027f6ec09
Brett Jarvis <brettbjarvis@gmail.com>
2023-02-01 10:39:20 -0800

    add fxnality to 5-useState-counter

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -3,18 +3,42 @@ import React, { useState } from 'react'
 const UseStateCounter = () => {
   const [count, setCount] = useState(0)
 
+  const decrease = () => {
+    setCount(count - 1)
+  }
+
+  const reset = () => {
+    setCount(0)
+  }
+
+  const increase = () => {
+    setCount(count + 1)
+  }
+
+  const increaseLater = () => {
+    setTimeout(increase, 3000)
+  }
+
   return (
     <>
       <h2>Regular Counter</h2>
       <h1>{count}</h1>
-      <button className='btn'>Decrease</button>
-      <button className='btn'>Reset</button>
-      <button className='btn'>Increase</button>
+      <button className='btn' onClick={decrease}>
+        Decrease
+      </button>
+      <button className='btn' onClick={reset}>
+        Reset
+      </button>
+      <button className='btn' onClick={increase}>
+        Increase
+      </button>
       <h1></h1>
 
       <h2>More Complex Counter</h2>
       <h1>{count}</h1>
-      <button className='btn'>Increase Later</button>
+      <button className='btn' onClick={increaseLater}>
+        Increase Later
+      </button>
     </>
   )
 }
