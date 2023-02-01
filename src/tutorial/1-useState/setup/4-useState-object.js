498e7517b6c2e5f54a83973e27e3f5dd3da82d0e
Brett Jarvis <brettbjarvis@gmail.com>
2023-02-01 10:10:20 -0800

    add fxn to 4-useState-object instead of inline

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -6,15 +6,16 @@ const UseStateObject = () => {
     age: 24,
     message: 'random message',
   })
+
+  const changeMessage = () => {
+    setPerson({ ...person, message: 'hello world' })
+  }
   return (
     <>
       <h3>{person.name}</h3>
       <h3>{person.age}</h3>
       <h4>{person.message}</h4>
-      <button
-        className='btn'
-        onClick={() => setPerson({ ...person, message: 'hello worlk' })}
-      >
+      <button className='btn' onClick={changeMessage}>
         Change Message
       </button>
     </>
