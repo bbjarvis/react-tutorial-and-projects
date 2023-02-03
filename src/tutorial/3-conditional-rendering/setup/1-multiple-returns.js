f4a58a69b26762382919f2df8f870fe6a6eb96c3
Brett Jarvis <brettbjarvis@gmail.com>
2023-02-03 14:33:40 -0800

    add title change in  1-multiple-returns

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,13 +1,40 @@
 import React, { useState, useEffect } from 'react'
 const url = 'https://api.github.com/users/QuincyLarson'
 const MultipleReturns = () => {
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(true)
+  const [isError, setIsError] = useState(false)
+  const [user, setUser] = useState('default user')
 
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((user) => {
+        const { login } = user
+        setUser(login)
+        setLoading(false)
+      })
+      .catch((e) => console.log(e))
+  }, [])
   if (loading) {
-    return <h2>Loading...</h2>
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    )
+  }
+  if (isError) {
+    return (
+      <div>
+        <h1>Error...</h1>
+      </div>
+    )
   }
 
-  return <h2>multiple returns</h2>
+  return (
+    <div>
+      <h1>{user}</h1>
+    </div>
+  )
 }
 
 export default MultipleReturns
