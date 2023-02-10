c69dfee018a5ab88f650e21b35f370ebff5ee423
Brett Jarvis <brettbjarvis@gmail.com>
2023-02-09 17:14:09 -0800

    setup 2-short-circuit

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  // const firstValue = text || 'hello world'
+  // const secondValue = text && 'hello world'
+  const text = 'There Is No Error'
 
-  return <h2>short circuit</h2>;
-};
+  return (
+    <>
+      <h1>John Doe</h1>
+      <button className='btn'>Toggle Error</button>
+      {text && <h1>Error...</h1>}
+    </>
+  )
+}
 
-export default ShortCircuit;
+export default ShortCircuit
