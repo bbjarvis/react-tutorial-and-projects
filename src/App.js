21b5861e68ea5ee1c134f11b9f4d99573e4a88ae
Brett Jarvis <brettbjarvis@gmail.com>
2023-04-26 09:18:49 -0700

    testing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Setup from './tutorial/3-conditional-rendering/setup/1-multiple-returns'
-import Final from './tutorial/3-conditional-rendering/final/1-multiple-returns'
+// import Final from './tutorial/3-conditional-rendering/final/1-multiple-returns'
 function App() {
   return (
     <div className='container'>
       <Setup />
-      <Final />
+      {/* <Final /> */}
     </div>
   )
 }
